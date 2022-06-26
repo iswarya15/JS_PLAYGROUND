29acdcd2e15a45a2c47673e73ca13ed32987a557
Iswarya <85299439+iswarya15@users.noreply.github.com>
2022-06-27 00:28:51 +0530

    Update Block_Scope.js

--- Block_Scope.js
+++ Block_Scope.js
@@ -22,7 +22,7 @@ let a = 20;
 {
     var a = 20; // Syntax error - Even though var a is inside a block scope, var variables are function scoped
    //meaning if there is no function it is declared in Global Execution Context.
-   // let a = 20 is also in GLE. So it conflicts
+   // let a = 20 is also in GLE. Duplication of let and const variables within same execution context is not allowed. 
 
    let a = 40; //This will work because let, const are block scoped. They are in different EC. 
    //Once this block of code is finished executing, this EC is removed. 
